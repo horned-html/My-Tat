09da69fbe56268123fb42de6ad13c673ffeee316
Amber <32426479+horned-html@users.noreply.github.com>
2022-12-15 12:34:56 -0500

    Update date.js

--- docs/date.js
+++ docs/date.js
@@ -1,4 +1,4 @@
-const dateTat = new Date("1977-12-14"); // This will be the date when I get the tattoo. For now it's just a placeholder.
+const dateTat = new Date("1977-11-15"); // This will be the date when I get the tattoo. For now it's just a placeholder.
 const dateElement = document.getElementById("date");
 
 var dateNow = new Date(); 
@@ -7,26 +7,24 @@ var years = dateDiff.getFullYear() - 1970;
 var months = dateDiff.getMonth();
 var days = dateDiff.getDate() - 1;
 var dateString;
-var time = "";
 
-// Gart's Code (thank you btw)
+let time = []
 var yearUnit = `year${years > 1 ? "s" : ""},`;
 var monthUnit = `month${months > 1 ? "s" : ""},`;
 var dayUnit =  `day${days > 1 ? "s" : ""},`;
-// End of
+if (years > 0) time.push(`${years} ${yearUnit} `);
+if (months > 0) time.push(`${months} ${monthUnit} `);
+if (days > 0) time.push(`${days} ${dayUnit} `);
 
-// console.log("It's been " + years + " years, " + months + " months, and " + days + " days.");
-// dateString = "This tattoo is ";
-
-if (years != 0) time += `${years} ${yearUnit} `;
-if (months != 0) time += `${months} ${monthUnit} `;
-if (days != 0) time += `${days} ${dayUnit} `;
-
-// more gart code
-if (time.length > 0 && time.includes(",")) time = time.substring(0, time.lastIndexOf(","));
-if (time.length > 0 && time.includes(",")) time = time.substring(0, time.lastIndexOf(",")) + ", and" + time.substring(time.lastIndexOf(",") + 1);
 if (time.length === 0) time = "0 seconds";
-// endof
+if (time.length === 1) time = time[0];
+if (time.length === 2) time = time.join(" and ");
+if (time.length > 2) {
+    time[time.length - 1] = `and ${time[time.length - 1]}`;
+    time = time.join(", ");
+}
+
+console.log(time);
 
 dateString = `This tattoo is ${time} old`
 

dc0ddc9b7b580cb86e597e8bc370174b355e3493
Amber <32426479+horned-html@users.noreply.github.com>
2022-12-15 12:06:00 -0500

    Update date.js

--- docs/date.js
+++ docs/date.js
@@ -1,15 +1,32 @@
-const dateTat = new Date("2004-4-20"); // This will be the date when I get the tattoo. For now it's just a placeholder.
+const dateTat = new Date("1977-2-23"); // This will be the date when I get the tattoo. For now it's just a placeholder.
+const dateElement = document.getElementById("date");
+
 var dateNow = new Date(); 
 var dateDiff = new Date(dateNow - dateTat);
 var years = dateDiff.getFullYear() - 1970;
 var months = dateDiff.getMonth();
 var days = dateDiff.getDate() - 1;
-const dateElement = document.getElementById("date");
 var dateString;
 
-console.log("It's been " + years + " years, " + months + " months, and " + days + " days.");
-dateString = "It's been ";
-// console.log(dateTat.getMonth());
-// console.log(dateNow.getMonth());
+// Gart's Code (thank you btw)
+var yearUnit = `year${years > 1 ? "s" : ""}`;
+var monthUnit = `month${months > 1 ? "s" : ""}`;
+var dayUnit =  `day${days > 1 ? "s" : ""}`;
+// End of
+
+// console.log("It's been " + years + " years, " + months + " months, and " + days + " days.");
+dateString = "This tattoo is ";
+
+if (years != 0) {
+    dateString += `${years} ${yearUnit} `;
+}
+if (months != 0) {
+    dateString += `${months} ${monthUnit} `;
+}
+if (days != 0) {
+    dateString += `${days} ${dayUnit} `;
+}
+
+dateString += ` old`
 
 dateElement.innerHTML = dateString;

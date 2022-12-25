8e3a2f5d51755a78ec0f858f6e11fa81f4edc5b4
Amber <jayhrod26@gmail.com>
2022-12-25 06:17:19 -0500

    added a funny gif

--- docs/date.js
+++ docs/date.js
@@ -5,7 +5,7 @@ var dateNow = new Date();
 var dateDiff = new Date(dateNow - dateTat);
 var years = dateDiff.getFullYear() - 1970;
 var months = dateDiff.getMonth();
-var days = dateDiff.getDate();
+var days = dateDiff.getDate() - 1;
 var dateString;
 
 // Gart's code ( gurrrrrrett3 )

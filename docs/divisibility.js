b9d9f236566d8ddc2009fd3eb47d8649fb6fe8e5
Amber <jayhrod26@gmail.com>
2023-06-18 11:50:45 -0400

    Update divisibility.js

--- docs/divisibility.js
+++ docs/divisibility.js
@@ -5,20 +5,26 @@ const funFactText = document.getElementById("funFact");
 var presentDate = 0;
 var result = 0;
 var finalResult = 0;
-// var divisibleNumber = 0;
 var divisibleNumbers = [];
+var factString = "";
 
 presentDate = new Date();
 result = Math.round(presentDate.getTime() / (oneDay));
 finalResult = result.toFixed(0);
 
-for (let index = 1; index < finalResult; index++) {
-    if (finalResult % index == 0) {
-        divisibleNumbers.push(index);
-        // divisibleNumber = index;
+for (let i = 2; i < finalResult; i++) {
+    if (finalResult % i == 0) {
+        divisibleNumbers.push(i);
+        // divisibleNumber = i;
         // funFactText.innerHTML = "FUN FACT! Today's date is divisible by " + divisibleNumber + "!";
     }
 }
 
+if (divisibleNumbers.length > 1) {
+    funFactText.innerHTML = "FUN FACT! Today's date is divisible by ";
+    for (let j = 0; j < divisibleNumbers.length; j++) {
+        funFactText.innerHTML += divisibleNumbers[j] + ", ";
+    }
+}
 console.log(finalResult);
 console.log(divisibleNumbers);

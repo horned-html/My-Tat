20765867c286853961c8b14cbea59608c0515483
Amber <jayhrod26@gmail.com>
2023-06-20 13:55:15 -0400

    redesigned topnav

--- docs/menu.js
+++ docs/menu.js
@@ -1,9 +1,9 @@
-/* Set the width of the sidebar to 250px (show it) */
-function openNav() {
-    document.getElementById("mySidepanel").style.width = "50%";
-}
-
-/* Set the width of the sidebar to 0 (hide it) */
-function closeNav() {
-    document.getElementById("mySidepanel").style.width = "0";
-}
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  } 

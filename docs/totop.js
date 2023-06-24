e631ac5ccea03e642fd35192ff9ba1e9201d6622
Amber <jayhrod26@gmail.com>
2023-06-24 10:51:01 -0400

    smoothed scroll to top button

--- docs/totop.js
+++ docs/totop.js
@@ -14,6 +14,7 @@ function scrollFunction() {
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  // document.body.scrollTop = 0; // For Safari
+  // document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollIntoView({behavior: "smooth"});
 } 
